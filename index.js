0de3c6072f0a60b1319ddac5c09b8ad3e1cca202
gunesh <gunesh26@gmail.com>
2023-01-05 19:39:49 +0530

    1 file modified

--- index.js
+++ index.js
@@ -190,9 +190,14 @@ console.log("original string: " + dummyString)
 console.log("final string: " + finalString)
 
 var rrt = 'Product = "abcd def"';
-var query1 = rrt.replace(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g, '1');
-console.log(query1);
+// var query1 = rrt.replace(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g, '1');
+// console.log(query1);
 // var query2 = rrt.replace(/(?:[^\s"]+|"[^"]*")+/g, '2');
 // console.log(query2);
 // var query3 = rrt.replace(/(".*?"|[^"\s]+)+(?=\s*|\s*$)/g, '3');
 // console.log(query3);
+
+
+var outputn = rrt.replace(/"([^"]+)"/g, (_, g) => g.replace(',', '-'))
+
+console.log(output)
